e3e8425c71ebfd71aa5cc12abce84b23bfb85b13
24211a6792-PuneetKumarSoni <24211a6792@gmail.com>
2026-08-15 21:58:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 
 const API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
   ? 'http://localhost:5000/api'
-  : '/api';
+  : 'https://accialert-project.onrender.com/api';
 
 // Application State
 const state = {
